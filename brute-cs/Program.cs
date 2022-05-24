3026226d2dc06c0543aca6ae069097b7e927bb19
Susseratal <jtfjyrx@gmail.com>
2022-05-24 13:46:18 +0100

    cleared out cs version

--- brute-cs/Program.cs
+++ brute-cs/Program.cs
@@ -1,29 +1,13 @@
 ﻿using System;
 using Thread = System.Threading.Thread;
 
-namespace Processor
+namespace Brute
 {
     public class Program
     {
-        var rand = new Random();
-        string[] targetArray = ["H", "e" "l", "l", "o", ",", " ", "w", "o", "r", "l", "d"];
-        string[] stringArray = ["", "" "", "", "", "", "", "", "", "", "", ""];
-        int i = 0;
-
         static void Main()
         {
-            while (i < targetArray.Length)
-            {
-                if (stringArray[i] != targetArray[i])
-                {
-                    stringArray[i] = Convert.ToChar(rand.Next(32, 126));
-                }
-
-                else if (stringArray[i] == targetArray[i])
-                {
-                    ++i;
-                }
-            }
+            // yet to be done
         }
     }
 }
