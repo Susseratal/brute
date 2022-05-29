dc2c592797bc51a6b893de1a604e34bdf3f20969
Susseratal <jtfjyrx@gmail.com>
2022-05-29 20:15:37 +0100

    added arg parsing

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # What?
-This is a silly bit of fun to practice programming in different languages, and it's more interesting than writing normal hello world programs
+This is a silly bit of fun to practice programming in different languages, and it's more interesting than writing normal hello world programs  
+Usage:  
+
+        python brute.py/.cs -m message 
+        python brute.py/.cs -f file.txt
+
+Either of these should work
 
 This was inspired by [someone who wrote this in ruby](https://github.com/kickinespresso/brute_hello)
--- brute-py/brute.py
+++ brute-py/brute.py
@@ -2,13 +2,31 @@ import random
 import time
 import sys
 
-# it would be cool to make it read command line args - sys.argv just printed the directory, which was perplexing
-targetArray = list("Hello, I'm Iain and I'm very clever") # listify the string "hello world"
-stringArray = [""] * len(targetArray) # construct an array of empty strings the length of the target string
-i = 0 # empty variable
-while i < len(targetArray): # while i is less than the length of the target string
-    stringArray[i] = chr(random.randint(32, 126)) # Choose a random number and select the unicode character for it 
-    if stringArray[i] == targetArray[i]: # If it's equal to the corresponding character in the target string
-        i += 1 # increment i
-    print("".join(stringArray)) # not 100% on what this line does
-    time.sleep(0.05) # have a quick breather
+def brute(message):
+    targetArray = list(message) # listify the string "hello world"
+    stringArray = [""] * len(targetArray) # construct an array of empty strings the length of the target string
+    i = 0 # empty variable
+    while i < len(targetArray): # while i is less than the length of the target string
+        stringArray[i] = chr(random.randint(32, 126)) # Choose a random number and select the unicode character for it 
+        if stringArray[i] == targetArray[i]: # If it's equal to the corresponding character in the target string
+            i += 1 # increment i
+        print("".join(stringArray), end="", flush=True) # not 100% on what this line does
+        print(200*chr(8), end="")
+        time.sleep(0.05) # have a quick breather
+    print("\n")
+
+
+def main():
+    flag = str(sys.argv[1])
+    if flag == "-m":
+        text = str(sys.argv[2])
+        brute(text)
+    elif flag == "-f":
+        f = open(str(sys.argv[2]), "r")
+        fileText = f.read()
+        fileText = fileText.strip()
+        brute(ft)
+    else:
+        print("Error: unrecognised flag: " + str(sys.argv[1]))
+
+main()
