57ed1cf8ebefe2076ac306993124b5716b2792ec
Susseratal <jtfjyrx@gmail.com>
2022-06-06 22:45:18 +0100

    got the c# version working

---
+++ brute-cpp/brute.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+void brute() // decide whether to use char* or string because memory allocation
+
+int main(int argc, char** argv)
+{
+    // some cpp goes here
+}
--- brute-cs/Program.cs
+++ brute-cs/Program.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using Thread = System.Threading.Thread;
+using File = System.IO.File;
 
 namespace Brute
 {
     public class Program
     {
-        var rand = new Random();
 
         static void brute(string message)
         {
+            var rand = new Random();
             char[] targetArray = message.ToCharArray();
-            string[] stringArray = ([""] * targetArray.Length);
+            string[] stringArray = new string[targetArray.Length];
             int i = 0;
             while (i < targetArray.Length)
             {
-                char stringArray[i] = char(rand.Next(32, 126));
-                if (stringArray[i] == targetArray[i])
+                var currentChar = Convert.ToChar(rand.Next(32, 126));
+                Console.Write(currentChar);
+                if (currentChar == targetArray[i])
                 {
+                    Console.Write(" ");
                     ++i;
                 }
-                else
-                {
-                    // Console.WriteLine("".Join(stringArray), );
-                }
+                Console.Write(String.Join("", stringArray));
+                Console.Write("\b");
+                Thread.Sleep(5);
             }
+            Console.Write("\n");
         }
 
         static void Main(string[] args)
@@ -40,7 +43,6 @@ namespace Brute
 
             else
             {
-                Console.WriteLine(args.Length);
                 string flag = args[0];
                 if (flag == "-m")
                 {
@@ -50,7 +52,16 @@ namespace Brute
 
                 else if (flag == "-f")
                 {
-                    // read a file
+                    char[] toTrim = {'\n', '\r'};
+                    string f = args[1];
+                    string fileText = File.ReadAllText(@f);
+                    fileText = fileText.Trim(toTrim);
+                    brute(fileText);
+                }
+
+                else
+                {
+                    Console.WriteLine("The flag was not recognised, much like the confederate flag");
                 }
             }
         }
