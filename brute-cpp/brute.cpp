4a1c5dd3615d9c2ffd591bb5e3ed079478e57143
Iain <jtfjyrx@gmail.com>
2022-06-07 13:17:37 +0100

    got message working for c++ version

--- brute-cpp/brute.cpp
+++ brute-cpp/brute.cpp
@@ -1,14 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <cstring>
 #include <fstream>
 #include <chrono>
 #include <thread>
 
-void brute()
+void brute(std::string message)
 {
-    std::cout << "hello world\n";
+    srand (time(NULL));
+    int targetLen = message.length();
+    char targetArray[targetLen + 1]; // hah a ha allocate memory 
+    strcpy(targetArray, message.c_str());
+    std::string stringArray[targetLen];
+    int i = 0;
+    while (i < targetLen)
+    {
+        int currentGuess = rand() % 95 + 32; // range + minimum (because obviously that's what I'd do)
+        char currentChar = static_cast<char>(currentGuess);
+        std::cout << currentChar << std::flush;
+        if (currentChar == targetArray[i])
+        {
+            std::cout << " ";
+            i++;
+        }
+        std::cout << "\b";
+        std::this_thread::sleep_for(std::chrono::milliseconds(5)); // sleep for 5 ms
+    }
+    std::cout << "\n";
 }
 
-int main(int argc, char *argv[]) // fool, these are memory address pointers
+int main(int argc, char *argv[]) 
 {
     if (argc != 3)
     {
@@ -21,7 +43,8 @@ int main(int argc, char *argv[]) // fool, these are memory address pointers
         std::string flag = (argv[1]);
         if (flag == "-m")
         {
-            std::cout << "message\n";
+            std::string text = (argv[2]);
+            brute(text);
         }
         else if (flag == "-f")
         {
